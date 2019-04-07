ccd7109f5505b24dc99b4638a3555d43c2e41719
pjh4543 <pjh4543@gmail.com>
2019-04-07 14:24:23 +0900

    no message

---
+++ src/main/java/com/example/demo/webservice/JpaaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/webservice/web/WebRestController.java
@@ -0,0 +1,14 @@
+package com.example.demo.webservice.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+	
+	@GetMapping("/hello")
+	public String hell() {
+		return "hellow world";
+	}
+
+}
